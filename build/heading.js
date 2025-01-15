889659ed7efda148eeed6e9a458e66b8801234e4
Thehareo <hareo2003@gmail.com>
2025-01-14 16:32:58 -0800

    Updates

--- build/heading.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Heading() {
-  return /*#__PURE__*/React.createElement("div", {
-    id: "header"
-  }, /*#__PURE__*/React.createElement("h1", {
-    id: "keep"
-  }, "Keeper"));
-}
-export default Heading;
--- build/noteitem.js
+++
@@ -1,7 +0,0 @@
-let notes = [];
-export function getNotes() {
-  return notes;
-}
-export function addNote(note) {
-  notes.push(note);
-}
