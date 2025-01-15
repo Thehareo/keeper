c3d8495cf698eb62e5a9f3e6e4fabd2826f3206e
Thehareo <hareo2003@gmail.com>
2025-01-14 17:15:15 -0800

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-=======
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
 });
->>>>>>> 889659ed7efda148eeed6e9a458e66b8801234e4
